c5276e2e55be532b79b212fba10722a2bddb610f
nachool <nachma.olivia1394@gmail.com>
2024-02-27 18:50:52 +0700

    Add project files.

---
+++ Pertemuan1/Pertemuan1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+        float nJejari, nLuas;
+        cout << "Masukan jejari = ";
+        cin >> nJejari;
+        nLuas = 3.14 * nJejari * nJejari;
+        cout << "Luasnya = " << nLuas  << endl;
+    }
+}
+
